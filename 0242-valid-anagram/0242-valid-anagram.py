84e69fbabaeb18a064f7f56638df82a650d90f1c
indp03 <62676158+C0dingPaper@users.noreply.github.com>
2026-01-10 16:18:23 +0100

    Time: 15 ms (42.25%), Space: 20.4 MB (5.65%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # Create sets for both s and t and compare them 
-        # If they have the same values it returns true otherwise false
+        # Sort the strings and if they have different lengths we know its not an anagram
         return sorted(s) == sorted(t)
